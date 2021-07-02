e81293da9d6dc574964f06ba7a5331eb41a73450
BhuvanBezawada <bhuvan_777@outlook.com>
2021-07-02 16:07:41 +0100

    Created feedback box component with section heading as label

---
+++ src/main/java/view/FeedbackBox.java
@@ -0,0 +1,45 @@
+package view;
+
+import javax.swing.*;
+import javax.swing.border.EmptyBorder;
+import java.awt.*;
+
+public class FeedbackBox extends JPanel {
+
+    private String heading;
+    private JLabel headingLabel;
+    private JTextArea textPane;
+
+    public FeedbackBox(String heading) {
+        // Store heading
+        this.heading = heading;
+
+        // Setup components
+        setupLabel();
+        setupTextArea();
+
+        // Layout components from top to bottom on this panel
+        this.setLayout(new BorderLayout());
+
+        // Add components to the panel
+        this.add(headingLabel, BorderLayout.PAGE_START);
+        this.add(textPane, BorderLayout.CENTER);
+
+        // Add some padding to the bottom on the panel and make it visible
+        this.setBorder(new EmptyBorder(0, 20, 20, 20));
+        this.setVisible(true);
+    }
+
+    private void setupLabel() {
+        this.headingLabel = new JLabel(heading, SwingConstants.LEFT);
+        this.headingLabel.setBorder(new EmptyBorder(0, 0, 10, 0));
+    }
+
+    private void setupTextArea() {
+        this.textPane = new JTextArea();
+        this.textPane.setRows(10);
+        this.textPane.setBackground(Color.WHITE);
+        this.textPane.setEditable(true);
+        this.textPane.setLineWrap(true);
+    }
+}
