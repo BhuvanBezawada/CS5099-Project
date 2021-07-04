6874bc5cf5c3e0df7a0ed798bb24fb721b993d69
BhuvanBezawada <bhuvan_777@outlook.com>
2021-07-04 16:50:42 +0100

    Created helper class for borders

---
+++ src/main/java/view/BorderCreator.java
@@ -0,0 +1,16 @@
+package view;
+
+import javax.swing.border.Border;
+import javax.swing.border.EmptyBorder;
+
+public abstract class BorderCreator {
+
+    public static final int PADDING_10_PIXELS = 10;
+    public static final int PADDING_20_PIXELS = 20;
+
+    public static Border createEmptyBorder(int padding) {
+        return new EmptyBorder(padding, padding, 0, padding);
+    }
+
+
+}
