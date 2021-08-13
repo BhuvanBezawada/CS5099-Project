661cf85509a3165e399ed2a2017bbe3e3b99740f
BhuvanBezawada <bhuvan_777@outlook.com>
2021-08-13 18:02:02 +0100

    Added javadoc for NLP code

--- src/main/java/nlp/NLPPipline.java
+++ src/main/java/nlp/NLPPipline.java
@@ -1,37 +1,34 @@
 package nlp;
 
-import edu.stanford.nlp.coref.CorefCoreAnnotations;
-import edu.stanford.nlp.coref.data.CorefChain;
-import edu.stanford.nlp.ling.CoreAnnotations;
-import edu.stanford.nlp.ling.CoreLabel;
-import edu.stanford.nlp.pipeline.Annotation;
 import edu.stanford.nlp.pipeline.StanfordCoreNLP;
-import edu.stanford.nlp.semgraph.SemanticGraph;
-import edu.stanford.nlp.semgraph.SemanticGraphCoreAnnotations;
-import edu.stanford.nlp.trees.Tree;
-import edu.stanford.nlp.trees.TreeCoreAnnotations;
-import edu.stanford.nlp.util.CoreMap;
 
-import java.util.List;
-import java.util.Map;
 import java.util.Properties;
 
-public class BasicPipelineExample {
+/**
+ * NLP Pipeline
+ * Code adapted from: https://www.youtube.com/watch?v=pUwNTGyxIOc&list=PLK0V_H0fCvPjKcUPjdXNZ6k4LueCJjsBN&index=3
+ */
+public class NLPPipline {
 
+    // Instance variables
     private static StanfordCoreNLP stanfordCoreNLP;
-    private static Properties properties;
-    private static String propertiesNames = "tokenize, ssplit, parse, pos, lemma, ner, sentiment";
+    private static final Properties properties;
+    private static final String PROPERTY_NAMES = "tokenize, ssplit, parse, pos, lemma, ner, sentiment";
 
+    // Static initialisation of properties - only occurs once
     static {
         properties = new Properties();
-        properties.setProperty("annotators", propertiesNames);
+        properties.setProperty("annotators", PROPERTY_NAMES);
     }
 
+    /**
+     * Get the NLP pipeline.
+     * @return The NLP pipeline.
+     */
     public static StanfordCoreNLP getPipeline() {
         if (stanfordCoreNLP == null) {
             stanfordCoreNLP = new StanfordCoreNLP(properties);
         }
-
         return stanfordCoreNLP;
     }
 }
